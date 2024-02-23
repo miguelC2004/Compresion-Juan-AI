f797f230c63eea9c54b5a3bc415e2c8772cac799
Miguel Catano <118161067+miguelC2004@users.noreply.github.com>
2024-02-22 22:18:08 -0500

    Update compression_model.py

--- compression_model.py
+++ compression_model.py
@@ -1,8 +1,9 @@
 import tensorflow as tf
 
 class CompressionModel(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, input_shape=(None, None, 1)):
         super(CompressionModel, self).__init__()
+        self.input_layer = tf.keras.layers.Input(shape=input_shape)
         self.encoder = tf.keras.Sequential([
             tf.keras.layers.Conv2D(16, (3, 3), activation='relu', padding='same'),
             tf.keras.layers.MaxPooling2D((2, 2), padding='same'),
